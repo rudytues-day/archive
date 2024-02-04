ba6a741c4b99dc5a07f16326ab63a94e5dd0a07e
rudy tuesday <rudolfdienstag@gmail.com>
2024-02-03 20:46:17 -0500

    added projects and updated landing page
    also merged reset.css into new "utilities" folder and updated components and pages accordingly

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -19,6 +19,12 @@ export function slugify(text) {
     })
   }
 
+  export function formatDateYear(date) {
+    return new Date(date).toLocaleDateString('en-US', {
+      year: "numeric",
+    })
+  }
+
   // sort functions
 
 export function sortEntries(entries) {
@@ -29,4 +35,16 @@ export function sortEntries(entries) {
 
 export const sortByCreationDate = (a, b) => new Date(a) - new Date(b);
 export const sortByUploadDate = (a, b) => new Date(a.data.uploadDate).getTime() - new Date(b.data.uploadDate).getTime();
+
+export function sortBySimilar(entries) {
+  // get entries with current entry filtered out
+  // get tag array of current entry
+  // loop through entry tag arrays
+  // filter entries with some() or includes() at least one tag
+  // create counter function to count similar tags
+  // run entries through counter function
+  // if counter >= 3
+  // add to new array of similar entries
+  // return similar entries randomized
+}
 
