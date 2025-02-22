9d86844064f4a3a9e6e13e8a0e3e44c61794e2fa
rudy tuesday <rudolfdienstag@gmail.com>
2025-02-21 23:21:15 -0500

    removed Tags.astro
    - created generic "tags by frequency" function
    - moved tag styling to separate stylesheet and deleted Tags.astro

--- src/utility/scripts/sort-fxn.js
+++ src/utility/scripts/sort-fxn.js
@@ -1,25 +1,19 @@
 export const sortByCreationDate = (a, b) => new Date(a) - new Date(b);
 export const sortByUploadDate = (a, b) => new Date(b.data.uploadDate) - new Date(a.data.uploadDate);
 
-// sort tags by frequency
-// Tags.astro
-
-// export function tagsByFrequency(collection) {
-
-//     const allTags = collection.map((entry => 
-//         entry.data.tags)).reduce((accumulator, value) => accumulator.concat(value), [])
-
-//     let frequency = {}
-//     allTags.forEach(tag => {
-//         if (frequency[tag]) {
-//             frequency[tag]++;}
-//         else {frequency[tag] = 1}
-//     })
-
-//     function sortByFrequency(a, b) {
-//         return frequency[a] - frequency[b];
-//     }
-// }
-
-// const sortedTags = [... new Set(allTags.sort(sortByFrequency).toReversed().flat())];
-
+export function tagsByFrequency(collection) {
+	const allEntryTags = collection.map((entry => 
+		entry.data.tags)).reduce((accumulator, value) => 
+		accumulator.concat(value), [])
+	let frequency = {}
+	allEntryTags.forEach(tag => {
+		if (frequency[tag]) {
+			frequency[tag]++;}
+		else {frequency[tag] = 1}
+	})
+	function sortByFrequency(a, b) {
+		return frequency[a] - frequency[b];			
+	}
+	const sortedTags = [... new Set(allEntryTags.sort(sortByFrequency).toReversed().flat())];
+	return sortedTags;
+};
