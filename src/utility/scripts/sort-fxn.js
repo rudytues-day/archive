30a93cd5f7818c3a9ea4d60ab074fefa665b9198
rudy tuesday <rudolfdienstag@gmail.com>
2025-02-23 17:17:26 -0500

    askbox pages and styling
    - updated askbox layout and card to add slug link
    - temp updated to askbox/astro to slice question return
    - created question static paths
    - created return button function
    - created temp new sort by date fxn
    -

--- src/utility/scripts/sort-fxn.js
+++ src/utility/scripts/sort-fxn.js
@@ -1,6 +1,7 @@
 export const sortByCreationDate = (a, b) => new Date(b.data.createDate) - new Date(a.data.createDate);
 export const sortByUploadDate = (a, b) => new Date(b.data.uploadDate) - new Date(a.data.uploadDate);
 export const sortByAskDate = (a, b) => new Date(b.data.date) - new Date(a.data.date);
+export const sortByDate = (a, b) => new Date(b) - new Date(a);
 
 export function tagsByFrequency(collection) {
 	const allEntryTags = collection.map((entry => 
