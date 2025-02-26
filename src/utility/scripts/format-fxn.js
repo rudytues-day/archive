f66836ebcffde871c36b293d9b6784aebcf2883c
rudy tuesday <rudolfdienstag@gmail.com>
2025-02-25 23:58:13 -0500

    worked on askbox
    - paginated askbox and tag pages
    - updated static routes to remove askbox card component
    - created collapse function for questions over a certain length
    - created fun array of q&as

--- src/utility/scripts/format-fxn.js
+++ src/utility/scripts/format-fxn.js
@@ -34,27 +34,3 @@ export function sortEntries(entries) {
     allEntries.sort(sortFunction);
     return allEntries;
 }
-
-  // sort by similar
-
-//   function sortBySimilar(entries) {
-// 	const thisEntryTags = new Set(tags);
-// 	const otherEntries = entries.filter((entry =>
-// 		entry.slug !== slug));
-// 	const otherEntryTags = otherEntries.map((entry) => {
-// 		return new Set(entry.data.tags);
-// 	});
-// 	const similarTagCount = otherEntryTags.map((tagArray) => {
-// 		return intersection(thisEntryTags, tagArray)});
-// 	const slugsAndTags = otherEntries.map((entry, index) => {
-// 		return { slug: entry.slug, tags: similarTagCount[index] };
-// 	});
-// 	const sortedSets = slugsAndTags.sort((a, b) => b.tags.size - a.tags.size);
-// 	console.log(sortedSets)
-// 	const similarEntries = sortedSets.map((set) => {
-// 		const similar = otherEntries.find((entry) => entry.slug === set.slug);
-// 		return similar;
-// 	});
-// 	return similarEntries;
-// };
-
