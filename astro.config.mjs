b92427cb95f465adb9a0ee18a24e6715aae5eab6
rudy tuesday <rudolfdienstag@gmail.com>
2025-04-26 19:09:30 -0400

    styled news layout and added pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rudytues.day',
   integrations: [],
   devToolbar: {
-    enabled: true
+    enabled: false
   }
 });
 
