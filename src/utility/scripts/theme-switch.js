e7b98ae26435c740c053ac33bb68b81fb5b50157
rudy tuesday <rudolfdienstag@gmail.com>
2025-05-11 15:27:32 -0400

    implemented dark mode
    - removed custom variables from reset and created separate stylesheet
    - created new variable format and updated all variables across the site
    - deleted other variable components etc.
    - created theme switch button

---
+++ src/utility/scripts/theme-switch.js
@@ -0,0 +1,12 @@
+export function themeSwitch() {
+
+if (window.matchMedia && 
+    window.matchMedia('(prefers-color-scheme: dark)').matches &&
+    !localStorage.hasOwnProperty('theme')) {
+        document.documentElement.setAttribute('data-theme', "dark");
+        localStorage.setItem("data-theme", "dark")
+} else {
+    let theme = localStorage.getItem("theme")
+    document.documentElement.setAttribute('data-theme', `${theme}`)
+}
+}
