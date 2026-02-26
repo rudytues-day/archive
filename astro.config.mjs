3a1170045beecab4b68a74599b323776ff7fa9c7
rudy tuesday <127635894+rudytues-day@users.noreply.github.com>
2026-02-25 21:13:02 -0500

    code clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   integrations: [],
   devToolbar: {
     enabled: false
+
   }
 });
 
--- src/utility/scripts/theme-switch.js
+++ src/utility/scripts/theme-switch.js
@@ -1,12 +1,11 @@
 export function themeSwitch() {
-
-if (window.matchMedia && 
-    window.matchMedia('(prefers-color-scheme: dark)').matches &&
-    !localStorage.hasOwnProperty('theme')) {
-        document.documentElement.setAttribute('data-theme', "dark");
-        localStorage.setItem("data-theme", "dark")
-} else {
-    let theme = localStorage.getItem("theme")
-    document.documentElement.setAttribute('data-theme', `${theme}`)
-}
+    if (window.matchMedia && 
+        window.matchMedia('(prefers-color-scheme: dark)').matches &&
+        !localStorage.hasOwnProperty('theme')) {
+            document.documentElement.setAttribute('data-theme', "dark");
+            localStorage.setItem("data-theme", "dark")
+    } else {
+        let theme = localStorage.getItem("theme")
+        document.documentElement.setAttribute('data-theme', `${theme}`)
+    }
 }
